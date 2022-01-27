5af4cb5b4c00b6d52554eb079107768a26eac33d
Tartali <yrick.dunaizev.com@gmail.com>
2022-01-27 07:42:20 +0300

    Bruh

--- Color_Battle/registration/adapter.py
+++
@@ -1,8 +0,0 @@
-from allauth.account.adapter import DefaultAccountAdapter
-
-
-class MyAccountAdapter(DefaultAccountAdapter):
-
-    def get_signup_redirect_url(self, request):
-        path = "/profile/{username}"
-        return path.format(username=request.user.username)
