c26df97511503bd8305a8b492e9703fc2afc2fbd
Tartali <yrick.dunaizev.com@gmail.com>
2022-02-01 20:50:36 +0300

    commit

--- Color_Battle/registration/views.py
+++ Color_Battle/registration/views.py
@@ -6,7 +6,7 @@ from yookassa import Payment
 from yookassa.domain.notification import WebhookNotificationEventType, WebhookNotificationFactory
 
 from yookassa import Configuration, Payment
-
+from yookassa.domain.notification import WebhookNotificationEventType, WebhookNotificationFactory
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Sum
@@ -70,6 +70,11 @@ def black(request):
 
     idempotence_key = str(uuid.uuid4())
 
+    # payment_id = '29886c50-000f-5000-8000-113bdfdebe75'
+    # payment_one = Payment.find_one(payment_id)
+    #
+    # dict_payment = vars(payment_one)
+
     payment = Payment.create({
         "amount": {
             "value": "2.00",
@@ -80,38 +85,16 @@ def black(request):
         },
         "confirmation": {
             "type": "redirect",
-            "return_url": "https://www.merchant-website.com/return_url"
+            "return_url": "https://test-my-site-id.herokuapp.com/"
         },
+
+        "capture": True,
 
         "description": "Заказ №72"
     }, idempotence_key)
 
     confirmation_url = payment.confirmation.confirmation_url
 
-    if request.user.is_authenticated:
-        value, created = Choose.objects.get_or_create(voter=request.user)
-
-        if request.method == 'POST':
-            select_action = request.POST['choose']
-
-            if select_action == 'black':
-                value.count_black += 1
-                value.save()
-            # return redirect(url)
-
-    else:
-        if request.method == 'POST':
-            select_action = request.POST['choose']
-
-            if select_action == 'black':
-                return redirect("accounts/login")
-        return render(request, 'registration/black.html')
-
-    return render(request, 'registration/black.html', {"url": confirmation_url})
-
-
-def my_webhook_handler(request):
-    # Извлечение JSON объекта из тела запроса
     event_json = json.loads(request.body)
     try:
         # Создание объекта класса уведомлений в зависимости от события
@@ -124,6 +107,21 @@ def my_webhook_handler(request):
             }
             # Специфичная логика
             # ...
+            # if dict_payment['_PaymentResponse__status'] == 'succeeded':
+            value, created = Choose.objects.get_or_create(voter=request.user)
+
+            if request.method == 'POST':
+                select_action = request.POST['choose']
+
+                if select_action == 'black':
+                    value.count_black += 1
+                    value.save()
+                return redirect("home")
+
+            if value.count_white > 0 or value.count_black > 0 or value.count_purple > 0:
+                return render(request, '403/black.html')
+
+            return render(request, 'registration/black.html', {"url": confirmation_url})
         elif notification_object.event == WebhookNotificationEventType.PAYMENT_WAITING_FOR_CAPTURE:
             some_data = {
                 'paymentId': response_object.id,
@@ -192,6 +190,25 @@ def my_webhook_handler(request):
 
     return HttpResponse(status=200)  # Сообщаем кассе, что все хорошо
 
+    # # if dict_payment['_PaymentResponse__status'] == 'succeeded':
+    #     value, created = Choose.objects.get_or_create(voter=request.user)
+    #
+    #     if request.method == 'POST':
+    #         select_action = request.POST['choose']
+    #
+    #         if select_action == 'black':
+    #             value.count_black += 1
+    #             value.save()
+    #         return redirect("home")
+    #
+    #     if value.count_white > 0 or value.count_black > 0 or value.count_purple > 0:
+    #         return render(request, '403/black.html')
+    #
+    # else:
+    #     return render(request, 'registration/black_pay.html')
+    #
+    # return render(request, 'registration/black.html', {"url": confirmation_url})
+
 
 def black_results(request):
     # получаем всех голосовавших за черный цвет (1 или более раз)
@@ -202,7 +219,37 @@ def black_results(request):
 
 @login_required(login_url='accounts/login/')
 def white(request):
-    if request.user.is_authenticated:
+    Configuration.configure('873469', 'test_q_nwW-qQ3EihdW3M4NtbXgO4z9yGjMHVilhXbxfdXyY')
+    Configuration.configure_user_agent(framework=Version('Django', '3.1.7'))
+
+    idempotence_key = str(uuid.uuid4())
+
+    payment_id = '29886c50-000f-5000-8000-113bdfdebe75'
+    payment_one = Payment.find_one(payment_id)
+
+    dict_payment = {'_PaymentResponse__id': '29886c50-000f-5000-8000-113bdfdebe75', '_PaymentResponse__status': 'succeeded'}
+
+    payment = Payment.create({
+        "amount": {
+            "value": "2.00",
+            "currency": "RUB"
+        },
+        "payment_method_data": {
+            "type": "bank_card"
+        },
+        "confirmation": {
+            "type": "redirect",
+            "return_url": "https://test-my-site-id.herokuapp.com/"
+        },
+
+        "capture": True,
+
+        "description": "Заказ №72"
+    }, idempotence_key)
+
+    confirmation_url = payment.confirmation.confirmation_url
+
+    if dict_payment['_PaymentResponse__status'] == 'succeeded':
         value, created = Choose.objects.get_or_create(voter=request.user)
 
         if request.method == 'POST':
@@ -218,14 +265,9 @@ def white(request):
             return render(request, '403/white.html')
 
     else:
-        if request.method == 'POST':
-            select_action = request.POST['choose']
+        return render(request, 'registration/white_pay.html')
 
-            if select_action == 'white':
-                return redirect("accounts/login")
-        return render(request, 'registration/white.html')
-
-    return render(request, 'registration/white.html')
+    return render(request, 'registration/white.html', {"url": confirmation_url})
 
 
 def white_results(request):
@@ -237,8 +279,37 @@ def white_results(request):
 
 @login_required(login_url='accounts/login/')
 def purple(request):
+    Configuration.configure('873469', 'test_q_nwW-qQ3EihdW3M4NtbXgO4z9yGjMHVilhXbxfdXyY')
+    Configuration.configure_user_agent(framework=Version('Django', '3.1.7'))
 
-    if request.user.is_authenticated:
+    idempotence_key = str(uuid.uuid4())
+
+    payment_id = '29886c50-000f-5000-8000-113bdfdebe75'
+    payment_one = Payment.find_one(payment_id)
+
+    dict_payment = vars(payment_one)
+
+    payment = Payment.create({
+        "amount": {
+            "value": "2.00",
+            "currency": "RUB"
+        },
+        "payment_method_data": {
+            "type": "bank_card"
+        },
+        "confirmation": {
+            "type": "redirect",
+            "return_url": "https://test-my-site-id.herokuapp.com/"
+        },
+
+        "capture": True,
+
+        "description": "Заказ №72"
+    }, idempotence_key)
+
+    confirmation_url = payment.confirmation.confirmation_url
+
+    if dict_payment['_PaymentResponse__status'] == 'succeeded':
         value, created = Choose.objects.get_or_create(voter=request.user)
 
         if request.method == 'POST':
@@ -254,14 +325,8 @@ def purple(request):
             return render(request, '403/purple.html')
 
     else:
-        if request.method == 'POST':
-            select_action = request.POST['choose']
-
-            if select_action == 'purple':
-                return redirect("accounts/login")
-        return render(request, 'registration/purple.html')
-
-    return render(request, 'registration/purple.html')
+        return render(request, 'registration/purple_pay.html')
+    return render(request, 'registration/purple.html', {"url": confirmation_url})
 
 
 class Profile(TemplateView):
@@ -283,5 +348,3 @@ class Comment2(TemplateView):
         comment = Comment.objects.get(pk=1)
         context['comment'] = comment
         return context
-
-
